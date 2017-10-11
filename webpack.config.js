7a43c4fc2eda67dbc2902e276808616427642c78
Daniel Kiss <littlesnorrboy@gmail.com>
2017-10-11 11:03:00 +0200

    Add style and asset loading to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,38 @@
+var path = require('path');
+
+function root(args) {
+  args = Array.prototype.slice.call(arguments, 0);
+  return path.join.apply(path, [__dirname].concat(args));
+}
+
+var CopyWebpackPlugin = require('copy-webpack-plugin');
+
 module.exports = {
-    entry: "./src/main.ts",
+    entry: [
+        "./src/main.ts"
+        , "./assets/scss/styles.scss"
+    ],
     output: {
-        filename: "main.js",
+        filename: "[name].js",
         path: __dirname + "/dist/",
         libraryTarget: 'var',
         library: 'NicEdit'
     },
 
     resolve: {
-        extensions: [".ts", ".tsx", ".js"]
+        extensions: [".ts", ".tsx", ".js", ".css", ".scss"]
     },
+
+    plugins: [
+        new CopyWebpackPlugin([{
+            from: root('assets')
+        }])
+    ],
 
     module: {
         loaders: [
-            { test: /\.tsx?$/, loader: "awesome-typescript-loader" }
+            { test: /\.tsx?$/, loader: "ts-loader" },
+            { test: /\.(s?css|sass)$/, loader: 'style-loader!css-loader!sass-loader' },
         ]
     }
 };
